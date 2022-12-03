9914ed0290d25a4e6ca781699071f9607c6f585c
snhoa <snhoa16@naver.com>
2022-12-03 17:37:00 +0900

    Add heroku

--- EIP_Master/settings.py
+++ EIP_Master/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-1b7g1rp48go9(!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
